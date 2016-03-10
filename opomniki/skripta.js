928fc6e70af28906446acd8dc79dd2b782f27919
Matic Anžič <matic.anzic@gmail.com>
2016-03-10 14:25:01 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -6,14 +6,32 @@ window.addEventListener('load', function() {
 		var uporabnik = document.querySelector("#uporabnisko_ime").value;
 		document.querySelector("#uporabnik").innerHTML = uporabnik;
 		document.querySelector(".pokrivalo").style.visibility = "hidden";
-	}
+	};
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	
+	//Dodaj opomnik
+	var dodajOpomnik = function() {
+	
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value= "";
+		document.querySelector("#opomniki").innerHTML += " \
+			<div class='opomnik senca rob'> \
+				<div class='naziv opomnika'>" + naziv_opomnika + "</div> \
+				<div class='cas_opomnika'>Opomnik čez <span>" +
+				cas_opomnika + "</span> sekund.</div> \
+				</div>";
+			
+	};
+	
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
